e56ca8caf09d07e39cd8d1706e01d713d9026846
mkhuthir <mkhuthir@gmail.com>
2018-07-02 20:10:19 +0400

    progs: added result images

--- 2_A5_edge_detection_gradients/sobel_filter.m
+++ 2_A5_edge_detection_gradients/sobel_filter.m
@@ -1,3 +1,4 @@
+% Sobel Filter
 
 pkg load image;
 
@@ -6,10 +7,20 @@ filt = fspecial('sobel');
 
 % load image
 img = imread('../images/saturn.png');
-figure, imshow(img)
 
 % apply filter
 img_f = imfilter(double(img), filt);
-figure, imagesc(img_f);
+
+% show images
+figure(1, 'position',[100,100,size(img)(2)*2.5,size(img)(1)]);
+
+subplot(1,2,1);
+imshow(img)
+title("Gray Image");
+
+subplot(1,2,2);
+imagesc(img_f);
+axis('off');
 colormap gray;
+title("Sobel Filter");
 
