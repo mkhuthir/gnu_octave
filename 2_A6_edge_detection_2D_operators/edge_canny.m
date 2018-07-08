aa331e0d959ce08ca969c6caef1068f72ea31828
mkhuthir <mkhuthir@gmail.com>
2018-07-08 11:40:10 +0400

    progs: added 2_A6 examples

--- 2_A6_edge_detection_2D_operators/edge_canny.m
+++ 2_A6_edge_detection_2D_operators/edge_canny.m
@@ -14,13 +14,8 @@ h = fspecial('gaussian',[11,11], 4);
 % Apply filter to smooth image
 img_fltr = imfilter(img_gray, h);
 
-% Shift Image Right one pixel
-img_r = img_fltr;
-img_r(:,[2:(end)]) = img_r(:, [1:(end-1)]);
+% 
 
-% Shift Image Left one pixel
-img_l =img_fltr;
-img_l(:,[1:(end-1)]) = img_l(:, [2:end]);
 
 % Difference between right and left shifted images
 img_diff = double(img_r)-double(img_l);
