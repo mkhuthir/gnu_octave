4b207cc45478a3dc91a769914f05b532dce777e1
mkhuthir <mkhuthir@gmail.com>
2018-07-08 08:11:43 +0400

    progs: added 2_A6 images

--- 2_A6_edge_detection_2D_operators/for_your_eyes_only.m
+++ 2_A6_edge_detection_2D_operators/for_your_eyes_only.m
@@ -14,7 +14,7 @@ froomer_edge = edge(rgb2gray(froomer),'canny');
 common_edge = froomer_edge & frizzy_edge;
 
 % show images
-figure;
+figure(1, 'position',[100,100,size(frizzy)(2)*2.5,size(frizzy)(1)]);
 subplot(1,2,1);
 imshow(frizzy);
 title("frizzy Image");
@@ -23,7 +23,7 @@ subplot(1,2,2);
 imshow(frizzy_edge);
 title("frizzy edges");
 
-figure;
+figure(2, 'position',[100,100,size(froomer)(2)*2.5,size(froomer)(1)]);
 subplot(1,2,1);
 imshow(froomer);
 title("froomer Image");
