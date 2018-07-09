4d4d690b82055c19e310d4b0837e8a216c2f8189
mkhuthir <mkhuthir@gmail.com>
2018-07-09 21:12:21 +0400

    progs: 2_B1

--- 2_B1_hough_transform_lines/hough_demo.m
+++ 2_B1_hough_transform_lines/hough_demo.m
@@ -3,15 +3,18 @@
 pkg load image;
 
 % Read image
-img = imread('../images/flower1.jpg');
+img = imread('../images/shapes.jpg');
 
 % Convert to grayscale
 img_gray = rgb2gray(img);
 
-% Apply Canny filter
+% Apply Canny filter to find edges
 img_edge = edge(img_gray,'canny');
 
-figure(1);
-subplot(1,3,1), imshow(img)     , title('Original Image');
-subplot(1,3,2), imshow(img_gray), title('Image in grayscale');
-subplot(1,3,3), imshow(img_edge), title('Canny Filter');
+% Apply hough to find lines
+H = houghtf(img_edge);
+
+% Plot hough accumulator
+figure, imagesc(H),xlabel('X Data'),ylabel('Y data'),title('Hough accumulator')
+
+peaks = houghpeaks(H,100);
