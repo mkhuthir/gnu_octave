c514290f2b67f4e44e7c320f04966a1fda7184ce
mkhuthir <mkhuthir@gmail.com>
2018-07-09 15:20:51 +0400

    progs: added 2_B1

--- 2_B1_hough_transform_lines/hough_demo.m
+++ 2_B1_hough_transform_lines/hough_demo.m
@@ -1,1 +1,17 @@
 % Hough demo
+
+pkg load image;
+
+% Read image
+img = imread('../images/flower1.jpg');
+
+% Convert to grayscale
+img_gray = rgb2gray(img);
+
+% Apply Canny filter
+img_edge = edge(img_gray,'canny');
+
+figure(1);
+subplot(1,3,1), imshow(img)     , title('Original Image');
+subplot(1,3,2), imshow(img_gray), title('Image in grayscale');
+subplot(1,3,3), imshow(img_edge), title('Canny Filter');
